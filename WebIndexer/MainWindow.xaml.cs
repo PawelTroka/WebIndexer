3af482f2f577e44f1a2b46a53957fa66ec56900a
PTroka <pawel.troka@outlook.com>
2016-06-22 00:16:33 +0200

    Added waiting sound for PLSA.

--- WebIndexer/MainWindow.xaml.cs
+++ WebIndexer/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace WebIndexer
@@ -68,7 +71,15 @@ namespace WebIndexer
             startButton.IsEnabled = false;
             startButton_Copy.IsEnabled = false;
 
+            mp3Player.Open("Peon - praca praca.mp3");
+            mp3Player.Play(true);
             await _webCrawler.AnalyzeDocuments(int.Parse(slider_Copy1.Text),int.Parse(maxIterationsTextBox.Text),double.Parse(convergenceTextBox.Text), double.Parse(filterTextBox.Text));
+            mp3Player.Close();
+
+            mp3Player.Open("Work complete.mp3");
+            mp3Player.Play(false);
+            await Task.Delay(2000);
+            mp3Player.Close();
 
             startButton.IsEnabled = true;
             startButton_Copy.IsEnabled = true;
@@ -77,6 +88,47 @@ namespace WebIndexer
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             _webCrawler.Filter(double.Parse(filterTextBox.Text));
+        }
+
+
+        MP3Player mp3Player = new MP3Player();
+
+
+
+    }
+
+
+    public class MP3Player
+    {
+        private string _command;
+        private bool isOpen;
+        [DllImport("winmm.dll")]
+
+        private static extern long mciSendString(string strCommand, StringBuilder strReturn, int iReturnLength, IntPtr hwndCallback);
+
+        public void Close()
+        {
+            _command = "close MediaFile";
+            mciSendString(_command, null, 0, IntPtr.Zero);
+            isOpen = false;
+        }
+
+        public void Open(string sFileName)
+        {
+            _command = "open \"" + sFileName + "\" type mpegvideo alias MediaFile";
+            mciSendString(_command, null, 0, IntPtr.Zero);
+            isOpen = true;
+        }
+
+        public void Play(bool loop)
+        {
+            if (isOpen)
+            {
+                _command = "play MediaFile";
+                if (loop)
+                    _command += " REPEAT";
+                mciSendString(_command, null, 0, IntPtr.Zero);
+            }
         }
     }
 }
